2a78f15bd6b39a4c13d91029f2c9099e6f154df5
fillemon10 <filip.sjolander@gmail.com>
2020-08-27 10:47:54 +0200

    Update test.php

--- test.php
+++ test.php
@@ -12,7 +12,7 @@
     echo (strftime("%B %D %Y %X", mktime(10, 0, 0, 11, 21, 2004)) . "<br />");
     echo (strftime("%b %d %Y %X", mktime(20, 0, 0, 12, 31, 98)) . "<br />");
     echo (strftime("Today is %a on %b %d, %Y, %X time zone: %Z", time()));
-    echo 
+    echo (strftime("Hej HEj %T"));
     ?>
 </body>
 
