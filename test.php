29c53eedec58fead5d461e2f36c3ea9a075d84b1
fillemon10 <filip.sjolander@gmail.com>
2020-08-27 11:28:37 +0200

    d

--- test.php
+++ test.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test</title>
 </head>
-
 <body>
     <?php
     echo (strftime("%B %D %Y %X", mktime(10, 0, 0, 11, 21, 2004)) . "<br />");
     echo (strftime("%b %d %Y %X", mktime(20, 0, 0, 12, 31, 98)) . "<br />");
-    echo (strftime("Today is %a on %b %d, %Y, %X time zone: %Z", time()));
-    echo (strftime("Hej HEj %T"));
+    echo (strftime("Today is %a on %b %d, %Y, %X time zone: %Z", time()) . "<br/>");
+    $hej = "Hej";
+    echo "{$hej}san";
     ?>
 </body>
-
 </html>
